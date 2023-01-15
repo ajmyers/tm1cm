df9ac672c32e3dbfd2612bb0295ef56e4fa5c5be
Andrew Myers <me@ajmyers.net>
2023-01-15 10:43:11 -0600

    Update scaffold to support .sublime-package

--- src/tm1cm/scaffold.py
+++ src/tm1cm/scaffold.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import shutil
+import zipfile
 
 FOLDER_STRUCTURE = [
     'config',
@@ -37,17 +38,51 @@ def create_scaffold(path):
     try:
         logger.debug(__file__)
         include_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'include')
-        logger.debug(include_path)
-        for f in ['connect.yaml', 'tm1cm.yaml', 'credentials.yaml']:
-            copy_from = os.path.join(include_path, f)
-            copy_to = os.path.join(path, 'config/default', f)
-            logger.debug('copying: {} to {}'.format(copy_from, copy_to))
-            shutil.copy(copy_from, copy_to)
+
+        if '.sublime-package' in include_path:
+            pkg_path, pkg_suffix = include_path.split('.sublime-package' + os.sep)
+            pkg_path = pkg_path + '.sublime-package'
+
+            with zipfile.PyZipFile(pkg_path, mode="r") as zip_pkg:
+                fn = '.gitignore'
+                with zip_pkg.open(os.path.join(pkg_suffix, fn)) as zip_file:
+                    content = zip_file.read()
+                with open(os.path.join(path, fn), 'wb') as fp:
+                    fp.write(content)
+
+                for f in ['connect.yaml', 'tm1cm.yaml', 'credentials.yaml']:
+                    with zip_pkg.open(os.path.join(pkg_suffix, f)) as zip_file:
+                        content = zip_file.read()
+                    with open(os.path.join(path, 'config', 'default', f), 'wb') as fp:
+                        fp.write(content)
+
+                    if f == 'connect.yaml':
+                        with open(os.path.join(path, 'config', 'test', f), 'wb') as fp:
+                            fp.write(content)
+
+                        with open(os.path.join(path, 'config', 'prod', f), 'wb') as fp:
+                            fp.write(content)
+
+            return
 
         copy_from = os.path.join(include_path, '.gitignore')
         copy_to = os.path.join(path, '.gitignore')
         logger.debug('copying: {} to {}'.format(copy_from, copy_to))
         shutil.copy(copy_from, copy_to)
+
+        for f in ['connect.yaml', 'tm1cm.yaml', 'credentials.yaml']:
+            copy_from = os.path.join(include_path, f)
+            copy_to = os.path.join(path, 'config', 'default', f)
+            logger.debug('copying: {} to {}'.format(copy_from, copy_to))
+            shutil.copy(copy_from, copy_to)
+
+            if f == 'connect.yaml':
+                copy_to = os.path.join(path, 'config', 'test', f)
+                shutil.copy(copy_from, copy_to)
+
+                copy_to = os.path.join(path, 'config', 'prod', f)
+                shutil.copy(copy_from, copy_to)
+
     except Exception:
         raise
 
